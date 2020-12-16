d9b49aa3b0931bf93de2c77b4344a5ee1e821d14
kidtic <592113107@qq.com>
2020-12-16 21:18:03 +0800

    修改config 的样式

--- miniprogram/pages/config/config.js
+++ miniprogram/pages/config/config.js
@@ -5,14 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    avatarUrl: './user-unlogin.png',
+    userName: "none",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    if (!wx.cloud) {
+      wx.redirectTo({
+        url: '../chooseLib/chooseLib',
+      })
+      return
+    }
 
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              console.log(res.userInfo)
+              this.setData({
+                avatarUrl: res.userInfo.avatarUrl,
+                userInfo: res.userInfo,
+                userName: res.userInfo.nickName
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * getuserinfo按钮，返回函数
+   */
+  onGetUserInfo: function(e) {
+    if (!this.data.logged && e.detail.userInfo) {
+      this.setData({
+        logged: true,
+        avatarUrl: e.detail.userInfo.avatarUrl,
+        userInfo: e.detail.userInfo,
+        userName: e.detail.userInfo.nickName
+      })
+    }
   },
 
   /**
